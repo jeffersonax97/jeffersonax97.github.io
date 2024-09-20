9d8574fa7230e9ceb52fcd46a77c8f582fec63ab
Jefferson Xavier <132727611+jeffersonax97@users.noreply.github.com>
2024-09-20 20:09:54 -0300

    Upload da Pg Web
    Upload da página Web contendo o NOME e RA dos integrantes do grupo, mantendo em ordem alfabetica e adicionando o perfil do Linkedin em cada um.

---
+++ script.js
@@ -0,0 +1,7 @@
+// Exemplo de uma função que poderia ser adicionada
+function mostrarMensagem() {
+    alert("Bem-vindo ao grupo de estudos!");
+}
+
+// Chamando a função ao carregar a página
+window.onload = mostrarMensagem;
